fd98fb3ae4b484142cb0d347ced0b4628a602b49
AlshimaaSha <90255800+AlshimaaSha@users.noreply.github.com>
2025-03-07 01:16:35 +0200

    Update product.Route.js

--- Modules/Product/product.Route.js
+++ Modules/Product/product.Route.js
@@ -7,23 +7,13 @@ import { createProduct ,updateProduct,deleteProduct, getProductsByNameOrPrice, g
 
 productRoutes.post("/create", createProduct);
 
-
-// productRoutes.get("/product/search/:name", getProductById);
-
-
-
 productRoutes.get("/search",getProductsByNameOrPrice) //url in post man http://localhost:3000/search?keyword=productName&minPrice=10&maxPrice=100
 
 productRoutes.get("/product/:id", getProductById);
 productRoutes.get("/all", getAllProduct);
 
-
  productRoutes.put("/update/:id", updateProduct);
 
  productRoutes.delete("/delete/:id", deleteProduct);
-
-
-//,,,getProducts
-
 
 export default productRoutes;
