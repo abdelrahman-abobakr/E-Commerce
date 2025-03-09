b5094aaab5748595d246d60a5684148ef74c00c7
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-03-09 13:49:09 +0200

    handling payment

--- index.js
+++ index.js
@@ -6,30 +6,29 @@ import userRoutes from "./Modules/User/user.Route.js";
 import adminRoutes from "./Modules/Admin/admin.Route.js";
 
 import  productRoutes  from "./Modules/Product/product.Route.js";
-// import paymentRoutes from "./Modules/Payment/payment.Route.js";
+
 
 import session from "express-session";
 import passport from "passport"; 
 import "./Config/passport.js";
 
 import Stripe from 'stripe';
-
 import bodyParser from 'body-parser';
 import path from 'path';
 import paymentRoutes from './Modules/Payment/strip.Route.js';
 
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const app = express();
 
 app.use(express.json());
-app.use(userRoutes);
 
+app.use(userRoutes);
 app.use(productRoutes);
 app.use(orderRoutes)
 app.use(adminRoutes);
-
-// app.use(userRoute);
+app.use(paymentRoutes);
 
 // Session Middleware (required for Passport)
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
@@ -39,7 +38,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-app.use(paymentRoutes);
+
 
 const stripe = Stripe('EOd2IiJ3NnDiELC9zaFw_xz88FXyXwX5f6RC_f2GybbANIROnF0DXrdGnhLvm-fVer3AFT_VZciGJKwr');
 // Middleware setup
