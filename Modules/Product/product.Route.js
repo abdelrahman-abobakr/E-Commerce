1804e8c4939829df1b264b3c35cfde65897c2955
AlshimaaSha <alshimaashahat21@gmail.com>
2025-03-26 02:50:21 +0200

    add pagination on product

--- Modules/Product/product.Route.js
+++ Modules/Product/product.Route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct ,updateProduct,deleteProduct, getProductsByNameOrPrice, getAllProduct, getProductById} from "./product.Controller.js"
+import { createProduct ,updateProduct,deleteProduct, searchProducts, getAllProduct, getProductById} from "./product.Controller.js"
 import verifyToken from "../../Middleware/verifyToken.js"
 import {validateProduct} from "../../Middleware/productValidationMiddleware.js"
 import checkAdmin from "../../Middleware/checkAdmin.js";
@@ -8,7 +8,7 @@ const productRoutes = express.Router();
 productRoutes.use(verifyToken);
 
 productRoutes.post("/createproduct", checkAdmin, validateProduct, createProduct);
-productRoutes.get("/search",getProductsByNameOrPrice) 
+productRoutes.get("/search",searchProducts) //search by name or range of price
 productRoutes.get("/product/:id", getProductById);
 productRoutes.get("/all", getAllProduct);
 productRoutes.put("/update/:id", checkAdmin, updateProduct);
